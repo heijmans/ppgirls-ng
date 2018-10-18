a71979f80c6d67290f6a00dfc84962bb754ad738
Jan Heijmans <jan@egami.nl>
2018-10-18 15:21:13 +0200

    remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from "@angular/core";
-import { Store } from "@ngrx/store";
-import { Actions } from "@ngrx/effects";
-import { IState } from "./state/state";
 
 @Component({
   selector: "app-root",
@@ -9,16 +6,4 @@ import { IState } from "./state/state";
   styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = "ppgirls-ng";
-
-  constructor(actions$: Actions, store: Store<IState>) {
-    /*
-    actions$.subscribe((action) => {
-      console.log(action);
-    });
-    store.subscribe((state) => {
-      console.log(state);
-    });
-    */
-  }
 }
