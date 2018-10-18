f83e9d508b7ec2ce395948a2becfc1b54ff3ac96
Jan Heijmans <jan@egami.nl>
2018-10-18 10:08:23 +0200

    refactor effects

--- src/app/state/show.effects.ts
+++ src/app/state/show.effects.ts
@@ -1,46 +1,52 @@
 import { Injectable } from "@angular/core";
 import { Store } from "@ngrx/store";
 import { Actions, Effect } from "@ngrx/effects";
+import { Observable } from "rxjs";
 import { filter, map, switchMap, withLatestFrom } from "rxjs/operators";
 import {
+  AppAction,
+  IRequestShowsAction,
+  IRequestEpisodesAction,
   REQUEST_SHOWS,
   REQUEST_EPISODES,
   receiveShows,
   receiveEpisodes,
-  IRequestEpisodesAction,
-  IRequestShowsAction,
 } from "./actions";
 import { ShowService } from "./show.service";
 import { IState } from "./state";
 import { getShows, getEpisodes } from "./selectors";
 
-function fetchShows(showService: ShowService) {
-  return showService.getShows().pipe(map((data) => receiveShows(data)));
-}
-
-function fetchEpisodes(showService: ShowService, showId: number) {
-  return showService.getEpisodes(showId).pipe(map((data) => receiveEpisodes(showId, data)));
-}
-
 @Injectable()
 export class ShowEffects {
+  @Effect()
+  receiveShows$: Observable<AppAction>;
+
+  @Effect()
+  receiveEpisodes$: Observable<AppAction>;
+
   constructor(
     private actions$: Actions,
     private store: Store<IState>,
     private showService: ShowService,
-  ) {}
+  ) {
+    this.receiveShows$ = this.actions$.ofType<IRequestShowsAction>(REQUEST_SHOWS).pipe(
+      withLatestFrom(this.store),
+      filter(([_, state]) => !getShows(state)),
+      switchMap(() => this.fetchShows()),
+    );
 
-  @Effect()
-  receiveShows$ = this.actions$.ofType<IRequestShowsAction>(REQUEST_SHOWS).pipe(
-    withLatestFrom(this.store),
-    filter(([_, state]) => !getShows(state)),
-    switchMap(() => fetchShows(this.showService)),
-  );
+    this.receiveEpisodes$ = this.actions$.ofType<IRequestEpisodesAction>(REQUEST_EPISODES).pipe(
+      withLatestFrom(this.store),
+      filter(([{ showId }, state]) => !getEpisodes(showId)(state)),
+      switchMap(([{ showId }, _]) => this.fetchEpisodes(showId)),
+    );
+  }
 
-  @Effect()
-  receiveEpisodes$ = this.actions$.ofType<IRequestEpisodesAction>(REQUEST_EPISODES).pipe(
-    withLatestFrom(this.store),
-    filter(([{ showId }, state]) => !getEpisodes(showId)(state)),
-    switchMap(([{ showId }, _]) => fetchEpisodes(this.showService, showId)),
-  );
+  fetchShows() {
+    return this.showService.getShows().pipe(map((data) => receiveShows(data)));
+  }
+
+  fetchEpisodes(showId: number) {
+    return this.showService.getEpisodes(showId).pipe(map((data) => receiveEpisodes(showId, data)));
+  }
 }
