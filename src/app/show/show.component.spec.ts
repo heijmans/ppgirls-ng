6d721727c2d5ab66af1af5c222b8ab9d880e7355
Jan Heijmans <jan@egami.nl>
2018-10-19 09:33:27 +0200

    small refactor of show test

--- src/app/show/show.component.spec.ts
+++ src/app/show/show.component.spec.ts
@@ -30,76 +30,68 @@ describe("ShowComponent", () => {
     }).compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ShowComponent);
-    component = fixture.componentInstance;
-    store = TestBed.get(Store);
-    store.next(MOCK_STATE);
-    fixture.detectChanges();
+  describe("with show", () => {
+    beforeEach(() => {
+      fixture = TestBed.createComponent(ShowComponent);
+      component = fixture.componentInstance;
+      store = TestBed.get(Store);
+      store.next(MOCK_STATE);
+      fixture.detectChanges();
+    });
+
+    it("should get the showId, the show and the episodes", () => {
+      expect(component.showId).toEqual(5);
+      expect(component.show).toEqual(MOCK_SHOW);
+      expect(component.episodes).toEqual(MOCK_EPISODES);
+    });
+
+    it("request the show and the episodes", () => {
+      expect(store.dispatch).toHaveBeenCalledWith(fetchShows());
+      expect(store.dispatch).toHaveBeenCalledWith(fetchEpisodes(5));
+    });
+
+    it("should show the show", () => {
+      const element = fixture.nativeElement;
+      expect(element.querySelector(".details-title").textContent).toContain("PP2 (2013)");
+      expect(element.querySelector(".details-image").src).toContain("original.jpg");
+    });
+
+    it("should set the right properties on the episode list", () => {
+      const episodeList: EpisodeListMockComponent = fixture.debugElement.query(
+        By.directive(EpisodeListMockComponent),
+      ).componentInstance;
+      expect(episodeList.showId).toEqual(5);
+      expect(episodeList.episodes).toEqual(MOCK_EPISODES);
+    });
   });
 
-  it("should get the showId, the show and the episodes", () => {
-    expect(component.showId).toEqual(5);
-    expect(component.show).toEqual(MOCK_SHOW);
-    expect(component.episodes).toEqual(MOCK_EPISODES);
-  });
+  describe("without show", () => {
+    beforeEach(() => {
+      fixture = TestBed.createComponent(ShowComponent);
+      component = fixture.componentInstance;
+      TestBed.get(Store).next(MOCK_EMPTY_STATE);
+      fixture.detectChanges();
+    });
 
-  it("request the show and the episodes", () => {
-    expect(store.dispatch).toHaveBeenCalledWith(fetchShows());
-    expect(store.dispatch).toHaveBeenCalledWith(fetchEpisodes(5));
-  });
+    it("should get the showId, but not the show and the episodes", () => {
+      expect(component.showId).toEqual(6);
+      expect(component.show).toBeUndefined();
+      expect(component.episodes).toBeUndefined();
+    });
 
-  it("should show the show", () => {
-    const element = fixture.nativeElement;
-    expect(element.querySelector(".details-title").textContent).toContain("PP2 (2013)");
-    expect(element.querySelector(".details-image").src).toContain("original.jpg");
-  });
+    it("should not show the show", () => {
+      const element = fixture.nativeElement;
+      expect(element.querySelector(".details-title")).toBeNull();
+      expect(element.querySelector(".details-image")).toBeNull();
+    });
 
-  it("should set the right properties on the episode list", () => {
-    const episodeList: EpisodeListMockComponent = fixture.debugElement.query(
-      By.directive(EpisodeListMockComponent),
-    ).componentInstance;
-    expect(episodeList.showId).toEqual(5);
-    expect(episodeList.episodes).toEqual(MOCK_EPISODES);
-  });
-});
+    it("should show the loading message", () => {
+      const element = fixture.nativeElement;
+      expect(element.querySelector(".message").textContent).toContain("Loading shows...");
+    });
 
-describe("ShowComponent without show", () => {
-  let fixture: ComponentFixture<ShowComponent>;
-  let component: ShowComponent;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ShowComponent, SafeHtmlPipe, ShowTitlePipe, EpisodeListMockComponent],
-      providers: [{ provide: Store, useValue: new MockStore() }],
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ShowComponent);
-    component = fixture.componentInstance;
-    TestBed.get(Store).next(MOCK_EMPTY_STATE);
-    fixture.detectChanges();
-  });
-
-  it("should get the showId, but not the show and the episodes", () => {
-    expect(component.showId).toEqual(6);
-    expect(component.show).toBeUndefined();
-    expect(component.episodes).toBeUndefined();
-  });
-
-  it("should not show the show", () => {
-    const element = fixture.nativeElement;
-    expect(element.querySelector(".details-title")).toBeNull();
-    expect(element.querySelector(".details-image")).toBeNull();
-  });
-
-  it("should show the loading message", () => {
-    const element = fixture.nativeElement;
-    expect(element.querySelector(".message").textContent).toContain("Loading shows...");
-  });
-
-  it("should not show the episode list", () => {
-    expect(fixture.debugElement.query(By.directive(EpisodeListMockComponent))).toBeNull();
+    it("should not show the episode list", () => {
+      expect(fixture.debugElement.query(By.directive(EpisodeListMockComponent))).toBeNull();
+    });
   });
 });
