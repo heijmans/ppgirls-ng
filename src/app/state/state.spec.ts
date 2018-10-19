438369f97647758804f699a5f94c3e65de9e4434
Jan Heijmans <jan@egami.nl>
2018-10-19 15:58:46 +0200

    CustomRouteSerializer test

--- src/app/state/state.spec.ts
+++ src/app/state/state.spec.ts
@@ -6,7 +6,8 @@ import {
   IState,
   ISimpleRouteOptions,
 } from "./state";
-import { MOCK_STATE } from "./state.mock";
+import { CustomRouteSerializer } from "./state";
+import { ActivatedRouteSnapshot, Params, RouterStateSnapshot } from "@angular/router";
 
 describe("makeSimpleRouteState", () => {
   it("should make a route state with params", () => {
@@ -20,6 +21,33 @@ describe("makeSimpleRouteState", () => {
     expect(makeSimpleRouteState({ url: "/?test", queryParams: { showId: "5" } })).toEqual({
       state: { url: "/?test", params: {}, queryParams: { showId: "5" } },
       navigationId: 0,
+    });
+  });
+});
+
+function makeMockSnapshot(
+  params: Params,
+  queryParams: Params,
+  firstChild?: ActivatedRouteSnapshot,
+): ActivatedRouteSnapshot {
+  return ({ url: [], params, queryParams, firstChild } as any) as ActivatedRouteSnapshot;
+}
+
+describe("CustomRouteSerializer", () => {
+  it("should serialize route data", () => {
+    const routerState: RouterStateSnapshot = {
+      url: "/1",
+      root: makeMockSnapshot(
+        { a: "1" },
+        { a: "2" },
+        makeMockSnapshot({ b: "1" }, { b: "2" }, makeMockSnapshot({ c: "1" }, { c: "2" })),
+      ),
+    };
+    const serializer = new CustomRouteSerializer();
+    expect(serializer.serialize(routerState)).toEqual({
+      url: "/1",
+      params: { c: "1" },
+      queryParams: { a: "2" },
     });
   });
 });
