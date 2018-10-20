d71434bfabd8f8081eaf8499686c578e0f9c37f2
Jan Heijmans <jan@egami.nl>
2018-10-20 12:35:39 +0200

    rewrite show service with HttpClientTestingModule

--- src/app/state/show.service.spec.ts
+++ src/app/state/show.service.spec.ts
@@ -1,39 +1,52 @@
-import {HttpClient} from "@angular/common/http";
-import {async} from "@angular/core/testing";
-import {of} from "rxjs";
-import {MISSING_IMAGE, ShowService} from "./show.service";
-import {MOCK_EPISODES, MOCK_SHOW} from "./state.mock";
+import { async, TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { MISSING_IMAGE, ShowService } from "./show.service";
+import { MOCK_EPISODES, MOCK_SHOW } from "./state.mock";
 
 describe("ShowService", () => {
-  let httpClient: jasmine.SpyObj<HttpClient>;
+  let mockHttp: HttpTestingController;
   let showService: ShowService;
 
-  beforeEach(() => {
-    httpClient = jasmine.createSpyObj("httpClient", ["get"]);
-    showService = new ShowService(httpClient);
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [ShowService],
+      imports: [HttpClientTestingModule],
+    }).compileComponents();
+    mockHttp = TestBed.get(HttpTestingController);
+    showService = TestBed.get(ShowService);
+  }));
+
+  afterEach(() => {
+    mockHttp.verify();
   });
 
   it("should get the shows", async(() => {
-    httpClient.get.and.returnValue(of([{ show: MOCK_SHOW }]));
-    showService.getShows().subscribe(shows => {
+    showService.getShows().subscribe((shows) => {
       expect(shows).toEqual([MOCK_SHOW]);
     });
-    expect(httpClient.get).toHaveBeenCalledWith("https://api.tvmaze.com/search/shows?q=powerpuff");
+
+    const req = mockHttp.expectOne("https://api.tvmaze.com/search/shows?q=powerpuff");
+    expect(req.request.method).toBe("GET");
+    req.flush([{ show: MOCK_SHOW }]);
   }));
 
   it("should get the episodes", async(() => {
-    httpClient.get.and.returnValue(of(MOCK_EPISODES));
-    showService.getEpisodes(6).subscribe(episodes => {
+    showService.getEpisodes(6).subscribe((episodes) => {
       expect(episodes).toEqual(MOCK_EPISODES);
     });
-    expect(httpClient.get).toHaveBeenCalledWith("https://api.tvmaze.com/shows/6/episodes");
+
+    const req = mockHttp.expectOne("https://api.tvmaze.com/shows/6/episodes");
+    expect(req.request.method).toBe("GET");
+    req.flush(MOCK_EPISODES);
   }));
 
   it("should fix the missing show icon", async(() => {
-    httpClient.get.and.returnValue(of([{ show: { id: 8, name: "PP1", premiered: "2015" } }]));
-    showService.getShows().subscribe(shows => {
+    showService.getShows().subscribe((shows) => {
       expect(shows).toEqual([{ id: 8, name: "PP1", premiered: "2015", image: MISSING_IMAGE }]);
     });
-    expect(httpClient.get).toHaveBeenCalledWith("https://api.tvmaze.com/search/shows?q=powerpuff");
+
+    const req = mockHttp.expectOne("https://api.tvmaze.com/search/shows?q=powerpuff");
+    expect(req.request.method).toBe("GET");
+    req.flush([{ show: { id: 8, name: "PP1", premiered: "2015" } }]);
   }));
 });
